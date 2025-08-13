66699b9ce9ffd6b416bb634ea880117342cf35cf
Davronbek2811 <davronjon176@gmail.com>
2025-08-13 12:25:43 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+console.log(
+    typeof 'Salom'
+)
+
+console.log(
+    typeof 1
+)
+
+console.log(
+    typeof true
+)
+
+const name = prompt('stringni kiriting')
+const num = prompt('numberni kiriting ')
+const tru = prompt('boolean ni kiriting')
+
+alert('Xamma malumot tog`riligiga ishonchis komilmi?')
+
+alert(`Siz string ni ${name} Deb Kiritiz 
+numberni esa ${num} Deb kiritiz
+boolean esa ${tru} Deb kiritiz`)
